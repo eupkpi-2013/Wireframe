a99886ccee43599eb835d7f4d93b0a77105066c0
minnie pangilinan <minnie_pangilinan@yahoo.com.ph>
2013-05-09 11:15:03 +0800

    user css js stuff
    added popup sa submit for verification ng user

--- js.js
+++ js.js
@@ -2,17 +2,11 @@
 $(document).ready(function(){
 	$(".active").parent().show();
 	$(".accordion h3").click(function(){
-		$(this).parent().find($("ul")).slideToggle("slow");
+		$(this).parent().find($(".accordion-list")).slideToggle("slow");
 	});
 	// $(".addrowbutton").click(function(){
-		// $(".kpitable").add(
-		// "<tr><td>hello</td><td></td></tr>"
-		// );
-		// $(this).add("<p>addd</p>");
+		// $(".kpitable").append('<tr><td><input></input></td><td><select><option>Text</option>option>Integer</option><option>Boolean</option><option>Time Range</option></select></td></tr>');
 	// });
-	$(".addrowbutton").click(function(){
-		$(".kpitable").append('<tr><td><input></input></td><td><select><option>Text</option>option>Integer</option><option>Boolean</option><option>Time Range</option></select></td></tr>');
-	});
 	
 	$("#signup-button").click(function(){
 		$("#signup").slideToggle();
@@ -22,7 +16,15 @@ $(document).ready(function(){
 	$(function() {
 
 	var popup = false;
-
+	
+	$("#submitKPI-button").click(function(){
+		if(popup == false){
+			$("#overlayEffect").fadeIn();
+			$(".popup").fadeIn();
+			popup = true;
+		}
+		});
+	
 	$("#verifybutton").click(function(){
 		if(popup == false){
 			$("#overlayEffect").fadeIn();
@@ -33,31 +35,18 @@ $(document).ready(function(){
 		}
 		});
 
-		$("#close").click(function(){
-			hidePopup();
-		});
-		
-		$(".close").click(function(){
-			hidePopup();
-		});
+	$(".closepopup").click(function(){
+		hidePopup();
+	});
+	
+	$(".close").click(function(){
+		hidePopup();
+	});
 
-	function center(){
-		var windowWidth = document.documentElement.clientWidth;
-		var windowHeight = document.documentElement.clientHeight;
-		var popupHeight = $("#popupContainer").height();
-		var popupWidth = $("#popupContainer").width();
-		$("#popupContainer").css({
-			"position": "absolute",
-			"top": windowHeight/2-popupHeight/2,
-			"left": windowWidth/2-popupWidth/2
-		});
-
-		}
 	function hidePopup(){
 		if(popup==true){
-			$("#overlayEffect").fadeOut("slow");
-			$("#popupContainer").fadeOut("slow");
-			$("#close").fadeOut("slow");
+			$("#overlayEffect").fadeOut();
+			$(".popup").fadeOut();
 			popup = false;
 		}
 	}
